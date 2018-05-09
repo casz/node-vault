b8d58ca8f8dcc4ec5a6f1ba05da562e8353be1d3
Geoffrey Booth <webmaster@geoffreybooth.com>
2018-05-08 17:06:29 -0700

    Code style

--- src/aws-auth.js
+++ src/aws-auth.js
@@ -72,15 +72,12 @@ const getSignedEc2IamRequest = async () => {
   }
 }
 
-const awsEc2IamLogin = async (vault, options) => {
+const awsEc2IamLogin = async (vault, options = {}) => {
   // execute login operation
   const request = await getSignedEc2IamRequest()
   // the role to use with Vault might be different than the role used by AWS
-  if (options && options.vaultRole) {
-    request.role = options.vaultRole
-  } else if (process.env.VAULT_ROLE) {
-    request.role = process.env.VAULT_ROLE
-  }
+  request.role = options.vaultRole || process.env.VAULT_ROLE || request.role
+
   const authResult = await vault.awsIamLogin(request)
 
   // login with the returned token into node-vault
